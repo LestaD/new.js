a595b36114ce9d45929d87e9f721deadad4f4c38
Sergey Sova <i.am@lestad.net>
2016-06-08 22:55:06 +0300

    Add defaults and base question function

--- bin/new.js
+++ bin/new.js
@@ -3,13 +3,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const inquirer = require('inquirer');
 const chalk = require('chalk');
-const gitConfig = require('git-config');
 const optimist = require('optimist');
-const { merge } = require('lodash');
+const { pick, assign } = require('lodash');
 
 const Package = require('../package.json');
+const defaults = require('../lib/defaults');
+const { askUser } = require('../lib/questions');
 
 
 const argv = optimist
@@ -32,7 +32,7 @@ const argv = optimist
 
   .describe('version', 'Set initial package version')
   .string('version')
-  .default('version', '0.1.0')
+  .default('version', defaults.version)
 
   .describe('repo', 'Git repository (lestad/new.js)')
   .string('repo')
@@ -40,23 +40,22 @@ const argv = optimist
   .describe('bin', 'Create exec file ./bin/$name.js')
   .boolean('bin')
   .alias('bin', 'b')
-  .default('bin', false)
+  .default('bin', defaults.bin)
 
   .describe('main', 'Create library main file')
   .boolean('main')
   .alias('main', 'm')
-  .default('main', true)
+  .default('main', defaults.main)
 
   .describe('lic', 'Select license')
   .alias('lic', 'license')
-  .default('lic', 'MIT')
+  .default('lic', defaults.license)
 
   .describe('git', 'Initialize git repository')
   .boolean('git')
   .alias('git', 'g')
 
   .argv;
-
 
 
 if (argv.help) {
@@ -69,5 +68,11 @@ if (argv.ver) {
   process.exit(0);
 }
 
+const normalizedArgs = pick(argv, ['name', 'version', 'repo', 'bin', 'main', 'license', 'git']);
+let config = assign({}, defaults, normalizedArgs);
 
-console.log(argv);
+if (!argv.force) {
+  config = askUser(config);
+}
+
+console.log(config);

1a959620a1cef8e49e5c67d9a3cfc5f9522cf8dc
lestad <i.am@lestad.net>
2016-06-10 23:33:06 +0300

    Total refactoring
    * Move optimist to lib/commandline
    * Add list of licenses instead of input
    * Add doubledots after prompt message
    * Add some inquirer error messages
    * Remove bin and main questions
    * Add apptype to select bin and/or main
    * Init git repo changed to list instead of confirm
    * Add welcome message
    * Installed: license.js, observatory

--- bin/new.js
+++ bin/new.js
@@ -4,73 +4,18 @@ const fs = require('fs');
 const path = require('path');
 
 const chalk = require('chalk');
-const optimist = require('optimist');
 const { pick, assign } = require('lodash');
 
 const Package = require('../package.json');
+const commandLine = require('../lib/commandline');
 const defaults = require('../lib/defaults');
-const { init, askUser } = require('../lib/questions');
+const questions = require('../lib/questions');
 
-
-const argv = optimist
-  .usage('Usage: new [options]\n       new.js [options]\n       npm-new [options]')
-
-  .describe('h', 'Show help and usage information')
-  .alias('h', 'help')
-  .boolean('h')
-
-  .describe('v', 'Show new.js version')
-  .alias('v', 'ver')
-  .boolean('v')
-
-  .describe('f', 'Non interactive, use defaults')
-  .alias('f', 'force')
-  .boolean('f')
-
-  .describe('name', 'Set package name')
-  .string('name')
-
-  .describe('version', 'Set initial package version')
-  .string('version')
-  .default('version', defaults.version)
-
-  .describe('repo', 'Git repository (lestad/new.js)')
-  .string('repo')
-
-  .describe('bin', 'Create exec file ./bin/$name.js')
-  .boolean('bin')
-  .alias('bin', 'b')
-  .default('bin', defaults.bin)
-
-  .describe('main', 'Create library main file')
-  .boolean('main')
-  .alias('main', 'm')
-  .default('main', defaults.main)
-
-  .describe('lic', 'Select license')
-  .alias('lic', 'license')
-  .default('lic', defaults.license)
-
-  .describe('git', 'Initialize git repository')
-  .boolean('git')
-  .alias('git', 'g')
-
-  .argv;
-
-
-if (argv.help) {
-  optimist.showHelp();
-  process.exit(0);
-}
-
-if (argv.ver) {
-  console.log(`${Package.name} v${Package.version}`);
-  process.exit(0);
-}
+const argv = commandLine(defaults);
 
 const normalizedArgs = pick(argv, ['name', 'version', 'repo', 'bin', 'main', 'license', 'git']);
 let config = assign({}, defaults, normalizedArgs);
 
-init(config)
-.then(argv.force ? a => a : askUser)
+Promise.resolve(config)
+.then(argv.force ? a => a : questions)
 .then(config => console.log('Generate from:', config));

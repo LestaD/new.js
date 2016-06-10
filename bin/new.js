48e2c3c28d3ae32f0d54910e2e21c6476ea3ceea
lestad <i.am@lestad.net>
2016-06-11 01:00:27 +0300

    Add tasks queue

--- bin/new.js
+++ bin/new.js
@@ -10,12 +10,22 @@ const Package = require('../package.json');
 const commandLine = require('../lib/commandline');
 const defaults = require('../lib/defaults');
 const questions = require('../lib/questions');
+const generates = require('../lib/generates');
 
 const argv = commandLine(defaults);
 
-const normalizedArgs = pick(argv, ['name', 'version', 'repo', 'bin', 'main', 'license', 'git']);
+const normalizedArgs = pick(argv, [
+  'name',
+  'version',
+  'repo',
+  'bin',
+  'main',
+  'license',
+  'git'
+]);
 let config = assign({}, defaults, normalizedArgs);
 
 Promise.resolve(config)
 .then(argv.force ? a => a : questions)
-.then(config => console.log('Generate from:', config));
+// .then(config => console.log('Generate from:', config))
+.then(generates);
